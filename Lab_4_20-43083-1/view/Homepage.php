d84b68df2ffb0ae2a964d62d1263ddc82af5f98f
shshahadat <124811057+shshahadat@users.noreply.github.com>
2023-03-18 14:50:49 +0600

    Add files via upload

---
+++ Lab_4_20-43083-1/view/Homepage.php
@@ -0,0 +1,48 @@
+
+
+<head>
+	<title>Homepage</title>
+	<style>
+		nav{
+
+			background-color: Black;
+  			overflow: hidden;
+			align: center;
+		}
+		nav a {
+			float: left;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+			font-size: 17px;
+			font-family: sans-serif, Helvetica, Arial;
+		}
+
+		nav a:hover {
+			background-color: MintCream;
+			color: black;
+		}
+		
+	</style>
+</head>
+<body>
+
+<nav>
+	<a href="adminHome.php">Home</a>
+   
+    
+  
+</nav>
+
+<h1>Welcome to the Homepage</h1>
+<!-- s -->
+
+</body>
+</html>
+
+
+
+
+
+
